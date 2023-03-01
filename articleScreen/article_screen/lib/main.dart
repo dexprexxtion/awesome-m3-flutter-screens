e18fdb2038ea1562813017c4522dc2673a3a2f4e
Ketan Chowdhury <ketanchowdhury@protonmail.com>
2023-03-01 12:21:11 +0530

    Made the ArticleScreenPage

--- articleScreen/article_screen/lib/main.dart
+++ articleScreen/article_screen/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ArticleScreenApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ArticleScreenApp extends StatelessWidget {
+  const ArticleScreenApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +15,50 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
           fontFamily: 'Work Sans'),
+      home: ArticleScreenPage(),
+    );
+  }
+}
+
+class ArticleScreenPage extends StatelessWidget {
+  const ArticleScreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Lorem Ipsem',
+                style: TextStyle(
+                  fontFamily: 'Abril Fatface',
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'dolor sit amet consectetur',
+                style: TextStyle(
+                  fontFamily: 'Arbutus Slab',
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
     );
   }
 }
