92fc68fb8bb8cf3cc151daafdf10c556e15dcf70
Ketan Chowdhury <ketanchowdhury@protonmail.com>
2023-03-03 19:20:45 +0530

    PodcastPage made partly

--- podcastScreen/podcast_screen/lib/main.dart
+++ podcastScreen/podcast_screen/lib/main.dart
@@ -16,7 +16,65 @@ class PodcastScreenApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         fontFamily: 'Roboto'
       ),
+      home: PodcastPage(),
+    );
+  }
+}
+
+ class PodcastPage extends StatelessWidget {
+  const PodcastPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar.large(
+            title: Text(
+              'Overwhelming Regrets'
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {},
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: () {},
+              ),
+            ],
+            pinned: true,
+            backgroundColor: Theme.of(context).colorScheme.surface,
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                _buildPodcastCard(),
+                _buildPodcastCard(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
-}
+  Widget _buildPodcastCard() {
+    return Row(
+      children: <Widget>[
+
+      ],
+    );
+  }
+
+ }
